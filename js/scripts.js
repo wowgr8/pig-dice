a92ffc5a4ee958ed585a34f0799aa2eb1c9ccbda
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2021-12-06 14:55:09 -0800

    add working Player methods and UI Logic with roll and hold buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,59 @@
 // player business Logic
-function Player(playerName, currentScore, totalScore) {
+function rollDice() {
+  return Math.floor(Math.random() * 5) + 1;
+}
+
+function Player(playerName) {
   this.playerName = playerName;
-  this.currentScore = currentScore;
-  this.totalScore = totalScore;
+  this.currentScore = 0;
+  this.totalScore = 0;
 };
 
-let currentScore = 0
+Player.prototype.takeTurn = function(){
+  let dieResult = rollDice();
 
-Player.prototype.updatedTotalScore = function(currentScore) {
-  this.totalScore += currentScore;
-  if (thistory.totalScore >= 100) {
-    alert("You Win!")
+  if (dieResult >= 2 && dieResult < 7) {
+    this.currentScore += dieResult
+  } else {
+    this.currentScore = 0;
+    switchPlayer();
   }
-  if 
 }
 
-Player.prototype.displayUserScore = function(currentScore) {
-  
+Player.prototype.hold = function() {
+  this.totalScore += this.currentScore;
+  if (this.totalScore >= 25) {
+    alert("You won!");
+  } else {
+   this.currentScore = 0;
+   switchPlayer();
+  }
+}
+
+let playerOne = new Player("Player 1")
+let playerTwo = new Player("Player 2")
+let whoseTurn = playerOne
+
+function switchPlayer() {
+  if (whoseTurn === playerOne) {
+    whoseTurn = playerTwo
+  } else if (whoseTurn === playerTwo) {
+    whoseTurn = playerOne
+  }
 }
 
 
 
-//Dice logic
-  //Rolling Dice
-function rollDice() {
-  return Math.floor(Math.random() * 6) + 1;
-}
 
+// let currentScore = 0
 
-
-//let dice = Math.floor(Math.random() * 6) + 1;         
-
-
-
+// Player.prototype.updatedTotalScore = function(currentScore) {
+//   this.totalScore += currentScore;
+//   if (this.totalScore >= 100) {
+//     alert("You Win!")
+//   }
+//   if
+// }
 
 
 
@@ -45,10 +66,20 @@ $(document).ready(function() {
     const btnRoll = $(".btn-roll");
     const btnHide = $(".btn-hide");
     const btnNew = $(".btn-new");
+  });
 
-    
-    
+  $(".btn-hold").click(function() {
+    // call the hold method
+    whoseTurn.hold()
   })
+
+  $(".btn-roll").click(function() {
+    whoseTurn.takeTurn();
+  })
+
+  $("p").on({
+    mouseenter: function(){
+    $(this).css({"font-style": "italic", "font-weight": "bold","text-decoration": "underline"});
+    }
+  });
 })
-
-
