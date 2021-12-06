262320a5768a516d2a26c705b7c18eb5b9e3ed7e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2021-12-05 17:39:36 -0800

    add co-author
    Co-authored-by: Coy Griffin <coygriffi@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -22,13 +22,33 @@ Player.prototype.displayUserScore = function(currentScore) {
 
 
 //Dice logic
+  //Rolling Dice
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
 
-  let dice = Math.floor(Math.random() * 6) + 1;
+
+
+//let dice = Math.floor(Math.random() * 6) + 1;         
+
+
 
 
 
 
 //UI Logic
+$(document).ready(function() {
+  $("wrapper clearfix").submit(function(event) {
+    event.preventDefualt();
+    let player1 = $(".player1");
+    let player2 = $(".player2");
+    const btnRoll = $(".btn-roll");
+    const btnHide = $(".btn-hide");
+    const btnNew = $(".btn-new");
 
+    
+    
+  })
+})
 
 
